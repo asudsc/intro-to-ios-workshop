9adc4afdc31f261db3bfc66ce19f88354516f36a
ananay <i@ananayarora.com>
2022-11-16 21:43:18 -0700

    Initial Commit

---
+++ fifaworldcup/fifaworldcupApp.swift
@@ -0,0 +1,17 @@
+//
+//  fifaworldcupApp.swift
+//  fifaworldcup
+//
+//  Created by Ananay Arora on 11/16/22.
+//
+
+import SwiftUI
+
+@main
+struct fifaworldcupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
